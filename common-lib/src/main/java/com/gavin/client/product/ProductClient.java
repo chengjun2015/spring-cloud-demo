c055229a2ff403945e2406211480fce5d7143a84
gavin-guo <igavin.guo@gmail.com>
2016-08-20 11:59:32 +0800

    modified

--- common-lib/src/main/java/com/gavin/client/product/ProductClient.java
+++ common-lib/src/main/java/com/gavin/client/product/ProductClient.java
@@ -2,16 +2,18 @@ package com.gavin.client.product;
 
 import com.gavin.domain.product.Product;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @FeignClient("product-service")
 public interface ProductClient {
 
     @RequestMapping(value = "/products/{product_id}", method = RequestMethod.GET)
     Product searchProductById(@PathVariable("product_id") Long productId);
+
+    @RequestMapping(value = "/products/list", method = RequestMethod.GET)
+    List<Product> searchProductsByIds(@RequestBody Long[] productIds);
 
     @RequestMapping(value = "/products/{product_id}/reserve", method = RequestMethod.PUT)
     Boolean reserve(@PathVariable("product_id") Long productId, @RequestParam("quantity") Integer quantity);
