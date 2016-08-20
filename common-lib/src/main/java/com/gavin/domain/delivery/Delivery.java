57bfc63d74d08f4202d247f28d6d0b168a1779e9
gavin-guo <igavin.guo@gmail.com>
2016-08-21 01:43:30 +0800

    modified

---
+++ common-lib/src/main/java/com/gavin/domain/delivery/Delivery.java
@@ -0,0 +1,93 @@
+package com.gavin.domain.delivery;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Date;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Delivery {
+
+    private Long id;
+
+    private Long orderId;
+
+    private Long accountId;
+
+    private Long carrierId;
+
+    private String trackingNumber;
+
+    @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date createdTime;
+
+    @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date modifiedTime;
+
+    @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date receiptTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Long getCarrierId() {
+        return carrierId;
+    }
+
+    public void setCarrierId(Long carrierId) {
+        this.carrierId = carrierId;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(Date modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+
+    public Date getReceiptTime() {
+        return receiptTime;
+    }
+
+    public void setReceiptTime(Date receiptTime) {
+        this.receiptTime = receiptTime;
+    }
+}
---
+++ common-lib/src/main/java/com/gavin/domain/payment/Payment.java
@@ -0,0 +1,95 @@
+package com.gavin.domain.payment;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Payment implements Serializable {
+
+    private Long id;
+
+    private Long orderId;
+
+    private Long accountId;
+
+    private Integer paymentMethod;
+
+    private Boolean paid;
+
+    @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date createdTime;
+
+    @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date modifiedTime;
+
+    @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date paidTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Integer getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(Integer paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Boolean getPaid() {
+        return paid;
+    }
+
+    public void setPaid(Boolean paid) {
+        this.paid = paid;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(Date modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+
+    public Date getPaidTime() {
+        return paidTime;
+    }
+
+    public void setPaidTime(Date paidTime) {
+        this.paidTime = paidTime;
+    }
+}
