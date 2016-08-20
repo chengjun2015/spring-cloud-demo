55f6894d4b939f6b7f9fd39544e44fad81d57ce2
gavin-guo <igavin.guo@gmail.com>
2016-08-20 14:19:27 +0800

    modified

---
+++ common-lib/src/main/java/com/gavin/exception/order/OrderException.java
@@ -0,0 +1,31 @@
+package com.gavin.exception.order;
+
+public class OrderException extends RuntimeException {
+
+    private String message;
+
+    private Throwable cause;
+
+    public OrderException(String message, Throwable cause) {
+        this.message = message;
+        this.cause = cause;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public Throwable getCause() {
+        return cause;
+    }
+
+    public void setCause(Throwable cause) {
+        this.cause = cause;
+    }
+}
