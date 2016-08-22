41282616d52c7d01996155eccedbdfc37253891d
gavin-guo <igavin.guo@gmail.com>
2016-08-22 23:49:19 +0800

    modified

--- common-lib/src/main/java/com/gavin/payload/PaidMessage.java
+++ common-lib/src/main/java/com/gavin/payload/PaidMessage.java
@@ -7,11 +7,21 @@ public class PaidMessage implements Serializable {
 
     private Long orderId;
 
+    private Long accountId;
+
     public Long getOrderId() {
         return orderId;
     }
 
     public void setOrderId(Long orderId) {
         this.orderId = orderId;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
     }
 }
