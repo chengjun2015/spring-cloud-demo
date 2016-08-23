f325c64fb85819619119c6887a543748d0b8607f
gavin-guo <igavin.guo@gmail.com>
2016-08-23 16:27:05 +0800

    modified

---
+++ common-lib/src/main/java/com/gavin/constant/CacheNameConsts.java
@@ -0,0 +1,14 @@
+package com.gavin.constant;
+
+public interface CacheNameConsts {
+
+    String CACHE_ACCOUNTS = "cache.accounts";
+
+    String CACHE_ORDERS = "cache.orders";
+
+    String CACHE_PRODUCTS = "cache.products";
+
+    String CACHE_PAYMENTS = "cache.payments";
+
+    String CACHE_DELIVERIES = "cache.deliveries";
+}
