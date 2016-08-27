10ba767f5d32b03c5ee9f273c8fa25fe8a5c1a08
gavin-guo <igavin.guo@gmail.com>
2016-08-27 19:49:29 +0800

    modified

--- common-lib/src/main/java/com/gavin/enums/PointActionEnums.java
+++ common-lib/src/main/java/com/gavin/enums/PointActionEnums.java
@@ -2,9 +2,10 @@ package com.gavin.enums;
 
 public enum PointActionEnums {
 
-    POINT_ACTION_GAIN(1),
-    POINT_ACTION_SPEND(2);
-
+    POINT_ACTION_REWARD(1),
+    POINT_ACTION_CONSUME(2),
+    POINT_ACTION_EXPIRE(3);
+    
     private Integer action;
 
     PointActionEnums(Integer action) {
