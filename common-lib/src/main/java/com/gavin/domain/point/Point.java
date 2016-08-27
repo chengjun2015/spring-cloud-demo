e7b48bda90a56eff77992816e36749ed833e0e90
gavin-guo <igavin.guo@gmail.com>
2016-08-27 23:09:06 +0800

    modified

--- common-lib/src/main/java/com/gavin/domain/point/Point.java
+++ common-lib/src/main/java/com/gavin/domain/point/Point.java
@@ -9,15 +9,29 @@ import java.util.Date;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Point implements Serializable {
 
+    private Long id;
+
     private Long accountId;
 
-    private Long amount;
+    private Integer amount;
+
+    private Long lockForOrderId;
+
+    private String expireDate;
 
     @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
     private Date createdTime;
 
     @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss")
     private Date modifiedTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Long getAccountId() {
         return accountId;
@@ -27,12 +41,28 @@ public class Point implements Serializable {
         this.accountId = accountId;
     }
 
-    public Long getAmount() {
+    public Integer getAmount() {
         return amount;
     }
 
-    public void setAmount(Long amount) {
+    public void setAmount(Integer amount) {
         this.amount = amount;
+    }
+
+    public Long getLockForOrderId() {
+        return lockForOrderId;
+    }
+
+    public void setLockForOrderId(Long lockForOrderId) {
+        this.lockForOrderId = lockForOrderId;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(String expireDate) {
+        this.expireDate = expireDate;
     }
 
     public Date getCreatedTime() {
