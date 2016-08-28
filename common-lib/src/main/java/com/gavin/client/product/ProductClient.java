895978727458fa3eaaac561eb9b99f9cefcdf95e
gavin-guo <igavin.guo@gmail.com>
2016-08-28 10:02:05 +0800

    modified

--- common-lib/src/main/java/com/gavin/client/product/ProductClient.java
+++ common-lib/src/main/java/com/gavin/client/product/ProductClient.java
@@ -1,20 +1,17 @@
 package com.gavin.client.product;
 
 import com.gavin.domain.order.Item;
-import com.gavin.domain.product.Product;
+import com.gavin.model.order.OrderDetailModel;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.math.BigDecimal;
 
 @FeignClient("product-service")
 public interface ProductClient {
 
     @RequestMapping(value = "/products/reserve", method = RequestMethod.PUT)
-    BigDecimal reserve(@RequestBody Item[] items);
+    OrderDetailModel reserve(@RequestBody Item[] items);
 
     @RequestMapping(value = "/products/restore", method = RequestMethod.PUT)
     Boolean restore(@RequestBody Item[] items);
