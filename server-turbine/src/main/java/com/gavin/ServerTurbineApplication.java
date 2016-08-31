60ffdb2d71a3324c8dddb36e3829462a25318e45
gavin-guo <igavin.guo@gmail.com>
2016-09-01 00:49:35 +0800

    enable turbine stream

--- server-turbine/src/main/java/com/gavin/ServerTurbineApplication.java
+++ server-turbine/src/main/java/com/gavin/ServerTurbineApplication.java
@@ -2,10 +2,10 @@ package com.gavin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableTurbine
+@EnableTurbineStream
 public class ServerTurbineApplication {
 
     public static void main(String[] args) {
