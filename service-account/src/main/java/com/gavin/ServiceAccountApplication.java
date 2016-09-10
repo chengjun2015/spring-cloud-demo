c0d29b010b450e9c1dbe914fdd06ccce382a4c4f
gavin-guo <igavin.guo@gmail.com>
2016-09-10 15:14:44 +0800

    add oauth2 support.

--- service-account/src/main/java/com/gavin/ServiceAccountApplication.java
+++ service-account/src/main/java/com/gavin/ServiceAccountApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -17,10 +18,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableCaching
 @EnableCircuitBreaker
+@EnableResourceServer
 @MapperScan("com.gavin.dao")
 public class ServiceAccountApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceAccountApplication.class, args);
     }
+
+
 }
