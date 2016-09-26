753bef71c117b6d33d514b5491ce44f88cf74422
gavin-guo <igavin.guo@gmail.com>
2016-09-26 23:42:57 +0800

    changed to jwt token store.

--- server-zuul/src/main/java/com/gavin/ServerZuulApplication.java
+++ server-zuul/src/main/java/com/gavin/ServerZuulApplication.java
@@ -5,16 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableOAuth2Sso
-@EnableResourceServer
 public class ServerZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerZuulApplication.class, args);
     }
+
+//    @Bean
+//    RequestDumperFilter requestDumperFilter() {
+//        return new RequestDumperFilter();
+//    }
+
 }
